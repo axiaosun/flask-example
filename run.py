e219aea69f95a58415f936186bcfdb49adb51be9
Carter Adams <carteradams@gmail.com>
2018-09-04 14:13:02 -0400

    add new route 'development'

--- run.py
+++ run.py
@@ -7,5 +7,9 @@ def index():
     items = ['item one', 'item two', 'item three', 'item four']
     return(render_template("index.html", address="VARIABLE!", items=items))
 
+@app.route("/development")
+def development():
+    return "DEVELOPMENT!"
+
 if __name__ == "__main__":
     app.run("127.0.0.1", debug=True)
